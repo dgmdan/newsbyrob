e91fa854172dca05bc54f4333d6a7475556e8173
Dan Madere <dan.madere@cdk.com>
2026-03-15 21:56:21 -0400

    Use Gunicorn in production

--- scripts/start_web.sh
+++ scripts/start_web.sh
@@ -3,4 +3,9 @@ set -euo pipefail
 
 PORT=${PORT:-8000}
 python manage.py migrate --noinput
-exec python manage.py runserver 0.0.0.0:$PORT
+WEB_CONCURRENCY=${WEB_CONCURRENCY:-3}
+exec gunicorn newsbyrob_site.wsgi:application \
+    --bind 0.0.0.0:"$PORT" \
+    --workers "$WEB_CONCURRENCY" \
+    --access-logfile - \
+    --error-logfile -
